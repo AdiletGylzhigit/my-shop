35ce22bea2fed20b1210a14bda7faec3c1a4d70e
Adi Jigit <adijigit@gmail.com>
2023-03-22 11:54:08 +0600

    sitemap 2

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>JIGIT+</title>
         <meta
           name="google-site-verification"
           content="Z1y-jEXqrFFvb9uUB6DXBs1iHFTZNZDdscjZERY8GDo"
