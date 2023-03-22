c4e9d7491f9e404c656fd5db6b6d36193ba279d5
Adi Jigit <adijigit@gmail.com>
2023-03-22 21:04:51 +0600

    title google crawl

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>JIGIT+</title>
         <meta
           name="google-site-verification"
           content="Z1y-jEXqrFFvb9uUB6DXBs1iHFTZNZDdscjZERY8GDo"
